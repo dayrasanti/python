ba61a40043ca48dcd8aa74bff778cf37acbf9300
dayrasanti <dayranataly1999@gmail.com>
2024-10-16 11:17:00 -0500

    est

---
+++ aleatorio.py
@@ -0,0 +1,3 @@
+import random
+print(random.randint(3,9))
+print(random.choice([2,4,7,9,0,3]))
--- tiposdedatos/diccionario/diccionario.py
+++ tiposdedatos/diccionario/diccionario.py
@@ -1,18 +1,39 @@
-cliente={
-    "nombre":"Jorge",
-    "apellido":"rodrigez",
-    "edad":"30",
-    "telefono":"321876590",
-    "saldo":"5000"
-}
-print(cliente)
-valor = cliente.get("nombre")
-print(valor)
-cliente["email"] = "jorge.rodrigez@example.com"
-cliente["profesion"] = "ingeniero"
-print(cliente)
-print(cliente["telefono"])
-del cliente["email"]
-claves = cliente.keys()
-print(cliente.keys())
-print(cliente.values())
+# cliente={
+#     "nombre":"Jorge",
+#     "apellido":"rodrigez",
+#     "edad":"30",
+#     "telefono":"321876590",
+#     "saldo":"5000"
+# }
+# print(cliente)
+# valor = cliente.get("nombre")
+# print(valor)
+# cliente["email"] = "jorge.rodrigez@example.com"
+# cliente["profesion"] = "ingeniero"
+# print(cliente)
+# print(cliente["telefono"])
+# del cliente["email"]
+# claves = cliente.keys()
+# print(cliente.keys())
+# print(cliente.values())
+
+redes={}
+def menu():
+    while True:           
+        print("Banco")
+        print("ingrese 1 para agregar una red")
+        print("ingrese 2 para ver la red")
+        
+        opcion= int(input("ingresa la opcion"))
+        if opcion == '1':
+           print("opcion invalida")
+        elif opcion == '2':
+            print("opcion invalida")
+        else:
+            print("opcion invalida")
+def agregarclientes():
+    red=input("Nombre de red ")
+    clave=input("clave ")
+    redes.append(redes)
+    print("clave agregado")
+menu()
