6695a997675087c5ffd62ee4bdceee42d1f2f461
dayrasanti <dayranataly1999@gmail.com>
2024-10-16 12:02:39 -0500

    est

--- aleatorio.py
+++ aleatorio.py
@@ -1,3 +1,22 @@
+# import random
+# print(random.randint(3,9))
+# print(random.choice([2,4,7,9,0,3]))
 import random
-print(random.randint(3,9))
-print(random.choice([2,4,7,9,0,3]))
+print('Nombre')
+Nombre = input()
+numero = random.randint(1, 20)
+print(Nombre + ', estoy pensando en un número entre 1 y 20.')
+while True:
+    valor = input()
+    valor = int(valor)
+    if valor < numero:
+        print("frio")
+    if valor> numero:
+        print("muy caliente")
+    if valor == numero:
+        break
+if valor == numero:
+    print('¡Bien, ' + Nombre + '! ¡Has adivinado!')
+
+
+
