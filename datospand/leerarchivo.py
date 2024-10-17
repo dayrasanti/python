2b01fec5981de8d97383919164b08d68883ba76c
dayrasanti <dayranataly1999@gmail.com>
2024-10-17 11:29:00 -0500

    est

--- datospand/leerarchivo.py
+++ datospand/leerarchivo.py
@@ -5,4 +5,16 @@ df=pd.read_csv("poblacion.csv")
 # print(df.head())
 # print(df.describe())
 # print(df.tail())
-print(df.sample())
+# print(df.sample())
+# print(df[["Date", "COL", "ARG"]])
+
+# # filtrado de datos
+# filtro = df[df['COL'] > 30000]
+# print(filtro)
+# Añadir una columna
+
+
+# Eliminar una columna
+df = df.drop(columns=['ABW'])
+
+print(df)
