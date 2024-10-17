2391b4404ae634cd23b66968a1d2f4088e6279ea
dayrasanti <dayranataly1999@gmail.com>
2024-10-17 08:41:37 -0500

    est

--- datos/pruebanumpy.py
+++ datos/pruebanumpy.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-lista=[1,2,3,4,5,6,7,8]
-print(lista)
-array = np.array(lista)
-print(array)
+# lista=[1,2,3,4,5,6,7,8]
+# print(type(lista))
+# array = np.array(lista)
+# print(type(array))
+
+# sumas con listas 
+lista1=[1,2,3,4,5,6,7,8]
+lista2=[16,22,37,54,58,64,74,83]
+arr1 = np.array(lista1)
+arr2 = np.array(lista2)
+resultado = np.add(arr1,arr2)
+print(resultado)
