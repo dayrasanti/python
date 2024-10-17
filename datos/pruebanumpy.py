aae53f1aea55c5d9e87e05dbb46106f77f86a925
dayrasanti <dayranataly1999@gmail.com>
2024-10-17 09:07:14 -0500

    est

--- datos/pruebanumpy.py
+++ datos/pruebanumpy.py
@@ -1,14 +1,29 @@
 import numpy as np
-
 # lista=[1,2,3,4,5,6,7,8]
 # print(type(lista))
 # array = np.array(lista)
 # print(type(array))
 
-# sumas con listas 
-lista1=[1,2,3,4,5,6,7,8]
-lista2=[16,22,37,54,58,64,74,83]
-arr1 = np.array(lista1)
-arr2 = np.array(lista2)
-resultado = np.add(arr1,arr2)
-print(resultado)
+# operaciones con listas 
+# lista1=[1,2,3,4,5,6,7,8]
+# lista2=[16,22,37,54,58,64,74,83]
+# arr1 = np.array(lista1)
+# arr2 = np.array(lista2)
+# resultado1 = np.add(arr1,arr2)
+# print(resultado1)
+# resultado2 = np.subtract(arr1,arr2)
+# print(resultado2)
+# resultado3 = np.multiply(arr1,arr2)
+# print(resultado3)
+# resultado4 = np.add(arr1,arr2)
+# print(resultado4)
+
+# funciones estadisticas
+data = np.array([1, 3, 3, 6, 5])
+
+# Calcular media
+media = np.median(data)
+print("media:",media)
+print("promedio:", np.mean(data))
+print("desviacion std:", np.std(data))
+
