91bd6420d8b98cacea15dc5f0d3c6c52d4dc9e51
dayrasanti <dayranataly1999@gmail.com>
2024-10-17 09:56:26 -0500

    est

--- datospand/pruebadataframes.py
+++ datospand/pruebadataframes.py
@@ -1,7 +1,13 @@
 import pandas as pd
 lista= [1,2,3,4,5,6,7,8,9]
 print(lista)
+nombre=["Ana","luis","carlos"]
+edad= [23,34,29]
+telefono= [123344,122323,24353454]
 # crear un Dataframe
 data = {"nombre":["Ana","luis","carlos"], "Edad":[23,34,29], "telefono":[123344,122323,24353454]}
 df=pd.DataFrame(data)
 print(df)
+data2= pd.DataFrame([nombre,edad,telefono])
+print(data2)
+df.to_csv("salida.csv",index=False)
