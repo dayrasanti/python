6167a6346ac1f8356925896586ef494def31e751
dayrasanti <dayranataly1999@gmail.com>
2024-10-17 10:46:41 -0500

    est

--- datospand/leerarchivo.py
+++ datospand/leerarchivo.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 df=pd.read_csv("poblacion.csv")
-print(df.head())
-print(df.describe())
-print(df.tail())
+# print(df.head())
+# print(df.describe())
+# print(df.tail())
 print(df.sample())
